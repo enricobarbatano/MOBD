Fixed implementation of f1, f2 and f3. Added f4 calculation through external program

main.py
@@ -1,17 +1,20 @@
 import numpy as np
 import obj 
-import matplotlib.pyplot as plt
 
 m=1000
-x0 =np.random.uniform(-100.0,100.0,size=(2,m))  # matrice 2xm con valori casuali tra -100 e 100
-print(x0)
+x0 =np.random.uniform(-100.0,100.0,size=(m,2))  # matrice 2xm con valori casuali tra -100 e 100
+
 np.savetxt('x.txt',x0.flatten(), fmt='%f')
-f1= obj.f1_tot(x0)
 
-f2= obj.f2_tot(x0)
-f3= obj.f3_tot_fast(x0)
-print(f1)
-print(f2)
-print(f3)
+f1 = obj.f1(x0)
 
-#12921.293207804929
+f2 = obj.f2(x0)
+
+f3 = obj.f3(x0)
+
+#f4 = obj.f4() #Uncomment this line if you want to compute f4, but be aware that it may take a long time to run.
+
+print('f1: ',f1)
+print('f2: ',f2)
+print('f3: ',f3)
+#print('f4: ',f4)

obj.py
@@ -1,84 +1,95 @@
+import subprocess
 import numpy as np
 import math as mt
-from decimal import getcontext
+from scipy.spatial.distance import pdist
 # definzione della funzione obiettivo sulla base dei costi f1, f2, f3  
 
 # dimensione = 1000
 m=1000
 
-r=100
-#np.linalg.norm
-def f1_i(xi,xj):
-    norm= mt.dist(xi,xj)
-    ret= 0.1 *(1-mt.exp(-(norm**2)))  
-    return ret
+r=100.0
 
-def f1_tot(x):
-    tot=0
-    for i in range(m):
-        xi = x[:,i]
+
+# -------------------------------------------------------------
+# f1
+# -------------------------------------------------------------
+
+def f1(x):
+    """
+    Calcola f1 usando NumPy e SciPy per la massima efficienza.
+    
+    Parametri:
+    x : numpy.ndarray
+        Matrice di forma (m, 2) contenente le coordinate spaziali (x_i^1, x_i^2) dei moduli.
         
-        for j in range(i+1,m):
-            tot+=f1_i(xi,x[:,j])
-           # print(tot)
-                
-    return tot
-def f2_i2(xi):
-    z0=np.array([0,0])
-    z1=np.array([100,100])
-    z2=np.array([-100,100])
-    z3=np.array([-100,-100])
-    z4=np.array([100,-100])
+    Ritorna:
+    float : Il costo totale di dispersione f1.
+    """
+    # pdist calcola le distanze per tutte le coppie non ordinate (i < j).
+    # 'sqeuclidean' calcola direttamente il quadrato della distanza euclidea: ||xi - xj||^2
+    distanze_quadrate = pdist(x, metric='sqeuclidean')
     
-    aux= (1/m) * (mt.dist(xi,z0)**2 + mt.dist(xi,z1)**2 + mt.dist(xi,z2)**2 + mt.dist(xi,z3)**2 + mt.dist(xi,z4)**2)
-    return aux
-
-LOG_DEN = np.log(1 + r**2)
-
-def f2_i(xi):
-
-   aux = (1/m)* (5*(mt.dist(xi,[0,0])**2) + 80000)
-   return aux
-
-def f2_tot(x):
-    tot = 0
-    for i in range(m):
-        tot += f2_i(x[:,i])
-    return tot
-
-def s_i(i):
+    # Applichiamo la formula 0.1 * (1 - e^(-||xi - xj||^2)) a tutte le distanze calcolate
+    dispersioni = 0.1 * (1 - np.exp(-distanze_quadrate))
     
-    si= (((-1)**i)* i * 0.2)
-    return np.array([si,si])
+    # f1 è la somma di tutte le dispersioni
+    f1 = np.sum(dispersioni)
+    return f1
 
-def f3_i(xi,i):
-    num=mt.log(1+ np.sum((xi-s_i(i))**2))
-    det=mt.log(1+ 10000)
-    aux=1000*((num/det) -1)**2    
-    return aux
+# -------------------------------------------------------------
+# f2
+# -------------------------------------------------------------
 
-def f3_tot(x):
-    tot=0
-    for i in range(m):
-        tot+=f3_i(x[:,i],i+1)
-    return tot
+def f2(x):
+    norma = np.sum(x**2, axis=1) # norma = prodotto scalare ** 2
+    aux = (1/m) * (5*norma + 80000)
+    f2 = np.sum(aux)
+    return f2
 
-def f3_tot_fast(x):
-    total = 0.0
-    for i in range(m):
-        si = ((-1)**(i+1)) * (i+1) * 0.2
-        si_vec = np.array([si, si])
-        
-        d2 = np.sum((x[:, i] - si_vec)**2)
-        val = (np.log(1 + d2) / LOG_DEN) - 1
-        total += 1000 * val**2
-        
-    return total
-def obj_base():
+# -------------------------------------------------------------
+# f3
+# -------------------------------------------------------------
+
+def f3(X):
+    """
+    Calcola la funzione di costo f3 per una matrice X di forma (m, 2).
+    """
+    # m è il numero di righe (moduli operativi)
+    m = X.shape[0] 
     
-    return 0
-
-def obj_tot():
+    # 1. Creiamo gli indici i = 1, ..., m (la formula matematica parte da 1)
+    I = np.arange(1, m + 1)
     
-    return 0
+    # 2. Calcoliamo i valori della formula per le coordinate di riferimento
+    # s_i = (-1)^i * i * 0.2
+    valori_s = ((-1.0)**I) * I * 0.2
+    
+    # Costruiamo la matrice S in modo che abbia la forma (m, 2)
+    # np.column_stack prende l'array 1D e lo mette in verticale come colonne
+    S = np.column_stack((valori_s, valori_s))
+    
+    # 3. Calcoliamo la distanza al quadrato ||x_i - s_i||^2
+    # Sottraiamo, eleviamo al quadrato e sommiamo lungo le colonne (axis=1)
+    # In questo modo "comprimiamo" le coordinate x e y ottenendo un array di m elementi
+    dist_quad = np.sum((X - S)**2, axis=1)
+    
+    # 4. Calcoliamo le costanti della formula
+    r = 100.0
+    denominatore = np.log(1 + r**2)
+    
+    # 5. Applichiamo la formula vettorialmente: 1000 * ((log(1 + D^2) / log(1 + r^2)) - 1)^2
+    deviazioni = 1000 * ((np.log(1 + dist_quad) / denominatore) - 1.0)**2
+    
+    # f3 è la somma totale di tutte le deviazioni
+    return np.sum(deviazioni)
+
+# -------------------------------------------------------------
+# f4
+# -------------------------------------------------------------
+
+def f4():
+    # Call external executable to compute f4. Only run this if needed, as it may be time-consuming.
+    result =subprocess.run([r'.\mobd.exe', 'x.txt', '-b'], capture_output=True, text=True)
+    return result.stdout
+    
 